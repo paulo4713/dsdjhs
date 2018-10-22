4ecb8ef8c1869772c91981fc266fe170aff8b642
paulo4713 <paulo4713@hotmail.com>
2018-10-21 20:19:04 -0500

    el mensaje del commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var express = require
+var fs = require('fs');
+var server = http.createServer(function(request,response){
+    switch(request.url){
+        case '/':
+        //var fs: typeof import("fs")
+        fs.createReadStream('./public/home.html').pipe(response);
+        //response.end('home');
+        break;
+        case '/about_us':
+        response.end('about us');
+        break;
+        case '/editado':
+        response.end('editado');
+        break;
+    }
+});
+
+server.listen(8000);
